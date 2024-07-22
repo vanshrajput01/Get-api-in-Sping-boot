bc65b411e9d3a9e67945e4889df132fd61aa1525
vanshrajput01 <rajputvansh009@gmail.com>
2024-07-22 10:39:27 +0530

    Api Repository Code add

--- src/main/java/com/example/demo/Repository/ApiRepository.java
+++ src/main/java/com/example/demo/Repository/ApiRepository.java
@@ -1,5 +1,16 @@
 package com.example.demo.Repository;
 
-public interface ApiRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.Entity.ApiEntityClass;
+
+public interface ApiRepository extends JpaRepository<ApiEntityClass, Integer>{
+
+	@Query(value = "select employeeid,firstname,lastname from employees where employeeid = :id",nativeQuery = true)
+	Object getdataInfo(@PathVariable("id") Integer id);
+	
+	
 
 }
--- src/main/java/com/example/demo/Response/ResponseUtils.java
+++ src/main/java/com/example/demo/Response/ResponseUtils.java
@@ -2,6 +2,9 @@ package com.example.demo.Response;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResponseUtils {
 	
 	public CommanResponse prepareResponse(Integer status,String message,Object data) {
